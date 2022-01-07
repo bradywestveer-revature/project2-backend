c0ed6176dba53dad346dd7410a7aca3abff3e434
David Helfer <dhelfer@atlassian.com>
2022-01-06 19:56:30 -0700

    postRepoTest added

---
+++ src/test/java/com/revature/project2backend/repositories/PostRepoTest.java
@@ -0,0 +1,22 @@
+package com.revature.project2backend.repositories;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostRepoTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void findByCreator() {
+    }
+}
