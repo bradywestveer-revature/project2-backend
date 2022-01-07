eeed4d17302eb01b2c217cf1c9a40ceae6e3d915
David Helfer <dhelfer@atlassian.com>
2022-01-06 19:59:04 -0700

    fixed name of postrepoTest

--- src/test/java/com/revature/project2backend/repositories/PostRepoIT.java
+++ src/test/java/com/revature/project2backend/repositories/PostRepoIT.java
@@ -4,9 +4,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-class PostRepoTest {
+class PostRepoIT {
 
     @BeforeEach
     void setUp() {
